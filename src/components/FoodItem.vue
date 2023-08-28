2de9d4e18bad9f53051ae3bc33f06cdd68f794e1
Josh Robinson <josh.robinson@gmail.com>
2023-08-28 09:55:45 -0400

    initial checkin

---
+++ src/components/FoodItem.vue
@@ -0,0 +1,44 @@
+<template>
+  <h2>{{ foodName }}</h2>
+  <p>{{ foodDesc }}</p>
+  <p v-show=foodIsFavorite>I'm a favorite!!!</p>
+  <button v-on:click="toggleFavorite">Favorite</button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      foodIsFavorite: this.isFavorite,
+    };
+  },
+  methods: {
+    toggleFavorite() {
+      this.foodIsFavorite = !this.foodIsFavorite;
+    },
+  },
+  props: {
+    foodName: {
+      type: String,
+      required: true,
+    },
+    foodDesc: {
+      type: String,
+      required: false,
+      default: "This is the default description.",
+      validator: function (value) {
+        let result = true;
+        if (value.length < 20 && value.length > 50) result = false;
+        return result;
+      },
+    },
+    isFavorite: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style></style>
