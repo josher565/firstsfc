13de4884c09fe9011398a3e92e5eda7ad6c5529a
Josh Robinson <josh.robinson@gmail.com>
2023-08-28 10:19:51 -0400

    using emits

--- src/App.vue
+++ src/App.vue
@@ -2,24 +2,40 @@
 export default {
   data() {
     return {
-        foods: [
-          { name: 'Apples',
-            desc: 'Apples are a type of fruit that grow on trees.',
-            favorite: true },
-          { name: 'Pizza',
-            desc: 'Pizza has a bread base with tomato sauce, cheese, and toppings on top.',
-            favorite: false },
-          { name: 'Rice',
-            desc: 'Rice is a type of grain that people like to eat.',
-            favorite: false }
-          { name: 'Fish',
-            desc: 'Fish is an animal that lives in water.',
-            favorite: true }
-          { name: 'Cake',
-            desc: 'Cake is something sweet that tastes good.',
-            favorite: false }
-        ]
-      };
+      foods: [
+        {
+          name: "Apples",
+          desc: "Apples are a type of fruit that grow on trees.",
+          favorite: true,
+        },
+        {
+          name: "Pizza",
+          desc: "Pizza has a bread base with tomato sauce, cheese, and toppings on top.",
+          favorite: false,
+        },
+        {
+          name: "Rice",
+          desc: "Rice is a type of grain that people like to eat.",
+          favorite: false,
+        },
+        {
+          name: "Fish",
+          desc: "Fish is an animal that lives in water.",
+          favorite: true,
+        },
+        {
+          name: "Cake",
+          desc: "Cake is something sweet that tastes good.",
+          favorite: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    receiveEvent(foodId) {
+      const foundFood = this.foods.find((food) => food.name === foodId);
+      foundFood.favorite = !foundFood.favorite;
+    },
   },
 };
 </script>
@@ -31,6 +47,7 @@ export default {
     :food-name="x.name"
     :food-desc="x.desc"
     :is-favorite="x.favorite"
+    @toggle-favorite="receiveEvent"
   />
 </template>
 
